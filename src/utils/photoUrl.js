36c9f37e0ee7b0cbbd8cff81fc730aafb2581d0d
Shubham Nikam <114049680+shubhamsnikam@users.noreply.github.com>
2025-10-12 19:58:01 +0530

    Update photoUrl.js

--- src/utils/photoUrl.js
+++ src/utils/photoUrl.js
@@ -1,5 +1,7 @@
-// photoUrl.js
+// Replace this with your real backend URL
+const BASE_URL = 'https://gym-server-1-nqw8.onrender.com';
+
 export const getPhotoUrl = (filename) => {
-    if (!filename) return 'https://via.placeholder.com/150?text=No+Image';
-    return `https://gym-server-1-nqw8.onrender.com/uploads/${filename}`;
+  if (!filename) return 'https://via.placeholder.com/150?text=No+Image';
+  return `${BASE_URL}/uploads/${filename}`;
 };
