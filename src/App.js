c6881dff672f34800f2a060721607fe367988904
Shubham Nikam <114049680+shubhamsnikam@users.noreply.github.com>
2025-10-12 20:02:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 // Components
 import NavBar from './components/NavBar';
@@ -11,14 +12,26 @@ import MemberDetails from './components/MemberDetails';
 import Home from './components/Home';
 import Help from './components/Help';
 
-
 function App() {
   return (
     <Router>
       <div className="App">
+        {/* Toastify Container (should exist only once in app) */}
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          pauseOnHover
+          draggable
+          theme="colored"
+        />
+
+        {/* Navbar and spacing */}
         <NavBar fixed="top" />
-      <div style={{ height: '32px' }} /> {/* spacer for navbar height */}
-        <NavBar />
+        <div style={{ height: '32px' }} /> {/* spacer for navbar height */}
+
         <div className="container mt-4">
           <Routes>
             <Route path="/" element={<Home />} />
@@ -27,8 +40,6 @@ function App() {
             <Route path="/members/edit/:id" element={<MemberForm />} />
             <Route path="/members/:id" element={<MemberDetails />} />
             <Route path="/help" element={<Help />} />
-            
-
           </Routes>
         </div>
       </div>
