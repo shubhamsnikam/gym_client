ed84b756f120022395a26c06b893ac2bc5e71ac8
Shubham Nikam <114049680+shubhamsnikam@users.noreply.github.com>
2025-10-18 13:14:58 +0530

    Update API calls to use backend URL from environment

--- src/services/api.js
+++ src/services/api.js
@@ -5,30 +5,30 @@ const REACT_APP_BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://local
 axios.defaults.baseURL = REACT_APP_BACKEND_URL;
 
 export const getMembers = async () => {
-  const res = await axios.get('/api/members');
+  const res = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/members`);
   return res.data;
 };
 
 export const getMemberById = async (id) => {
-  const res = await axios.get(`/api/members/${id}`);
+  const res = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/members/${id}`);
   return res.data;
 };
 
 export const createMember = async (formData) => {
-  const res = await axios.post('/api/members', formData, {
+  const res = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/api/members`, formData, {
     headers: { 'Content-Type': 'multipart/form-data' },
   });
   return res.data;
 };
 
 export const updateMember = async (id, formData) => {
-  const res = await axios.put(`/api/members/${id}`, formData, {
+  const res = await axios.put(`${process.env.REACT_APP_BACKEND_URL}/api/members/${id}`, formData, {
     headers: { 'Content-Type': 'multipart/form-data' },
   });
   return res.data;
 };
 
 export const deleteMember = async (id) => {
-  const res = await axios.delete(`/api/members/${id}`);
+  const res = await axios.delete(`${process.env.REACT_APP_BACKEND_URL}/api/members/${id}`);
   return res.data;
 };
